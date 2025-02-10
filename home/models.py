d0025074104adc6340915fa1a4f61d5d504e1797
sujataregmi12 <sujataregmi791@gmail.com>
2025-02-10 15:36:44 +0545

    update commit

--- home/models.py
+++ home/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Project(models.Model):
+    name= models.CharField(max_length=122)
+    short_description = models.CharField(max_length=122)
+    description = models.TextField(max_length=122)
+    created_date =models.DateTimeField()
+    end_date =models.DateTimeField()
+    TYPE_CHOICES = [
+        ('Web', 'Web Development'),
+      ]
+    type =models.CharField(max_length=12 ,choices=TYPE_CHOICES)
+    developers = models.ManyToManyField(User, related_name='projects')
+
+def __str__(self):
+        return self.name
--- home/views.py
+++ home/views.py
@@ -1,5 +1,24 @@
-from django.shortcuts import render , HttpResponse
-
+from django.shortcuts import render ,redirect
+from django.http import HttpResponse
+from home.models import Project
+from django.contrib.auth.models import User
+from datetime import datetime
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+        if request.method =='POST':
+            name = request.POST.get('name')
+            short_description = request.POST.get('short_description')
+            description = request.POST.get('description')
+            created_date = request.POST.get('created_date')
+            end_date = request.POST.get('end_date')
+            type = request.POST.get('type')
+            developer_ids = request.POST.getlist('developers')
+
+            project= Project(name=name, short_description=short_description,description=description,
+                          created_date=created_date,end_date=end_date,type=type)
+            for developer_id in developer_ids:
+                developer = User.objects.get(id=developer_id)  # Get the developer by ID
+                Project.developers.add(developer)
+            project.save()
+        return render(request, 'index.html')
+     
